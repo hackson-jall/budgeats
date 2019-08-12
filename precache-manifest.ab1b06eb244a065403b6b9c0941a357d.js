774eb1c2a2d8590d52563186babf344227838af4
Jackson Hall <jacksonhall@Jacksons-MacBook-Pro-2.local>
2019-08-12 17:54:32 -0400

    Updates

---
+++ precache-manifest.ab1b06eb244a065403b6b9c0941a357d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e737ec5f11660d9f3e8152ce1e15b987",
+    "url": "/budgeats/index.html"
+  },
+  {
+    "revision": "e5d865a62b0e880312e3",
+    "url": "/budgeats/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "57ede6d2915ff96e7be0",
+    "url": "/budgeats/static/js/2.f1cc7078.chunk.js"
+  },
+  {
+    "revision": "e5d865a62b0e880312e3",
+    "url": "/budgeats/static/js/main.c8392662.chunk.js"
+  },
+  {
+    "revision": "dc422795f9effe62919b",
+    "url": "/budgeats/static/js/runtime~main.46b086c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/budgeats/static/media/logo.5d5d9eef.svg"
+  }
+]);
